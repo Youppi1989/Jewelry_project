6122bbe92b59d51ba8f9a9a304f472f10a456c07
Youppi1989 <trotsenko_nika@mail.ru>
2023-10-16 14:56:50 +0200

    making JS slider

---
+++ src/sliderScript.js
@@ -0,0 +1,33 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slider img');
+const dots = document.querySelectorAll('.dot');
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.style.display = i === index ? 'block' : 'none';
+  });
+
+  dots.forEach((dot, i) => {
+    dot.classList.toggle('active', i === index);
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
+}
+
+document.querySelector('.left-arrow').addEventListener('click', prevSlide);
+document.querySelector('.right-arrow').addEventListener('click', nextSlide);
+
+dots.forEach((dot, index) => {
+  dot.addEventListener('click', () => showSlide(index));
+});
+
+// Show the first slide initially
+showSlide(currentSlide);
